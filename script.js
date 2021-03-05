093a04edbbf4242f76c32dcadc2b10f87f17a40c
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-03-05 22:46:50 +0300

    task 2

--- script.js
+++ script.js
@@ -8,12 +8,25 @@ const personalMovieDB = {
   privat: false
 };
 
-const filmOne = prompt('Один из последних просмотренных фильмов?', '');
-const filmOneRate = prompt('На сколько оцените его?', '');
-const filmTwo = prompt('Один из последних просмотренных фильмов?', '');
-const filmTwoRate = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const filmName = prompt('Один из последних просмотренных фильмов?', ''),
+    filmRate = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[filmOne] = filmOneRate;
-personalMovieDB.movies[filmTwo] = filmTwoRate;
+  if (filmName && filmName.length < 50 && filmRate) {
+    personalMovieDB.movies[filmName] = filmRate;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
