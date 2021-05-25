5180e46df4c139de1aa1196771aeb24496549738
Vitali Tumilovich <hey.tumilovich@gmail.com>
2021-05-25 17:09:59 +0300

    feat: add modal window open/close functional

--- js/script.js
+++ js/script.js
@@ -92,4 +92,38 @@ window.addEventListener('DOMContentLoaded', function() {
   };
 
   setClock('.timer', deadline);
+
+  // modal
+
+  const modalTriggers = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+  modalTriggers.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  const closeModal = () => {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  };
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (modal.classList.contains('show') && e.code === 'Escape') {
+      closeModal();
+    }
+  });
 });
